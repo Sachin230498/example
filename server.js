7ac29d4f2968cf2f9ed6a93eac31f7c39268b79b
Sachin230498 <sachinpathe230498@gmail.com>
2024-02-29 21:31:02 +0530

    s

--- server.js
+++ server.js
@@ -2,28 +2,24 @@
 
 const express = require("express");
 
-
-
+const cors = require("cors");
 
 const app = express();
 
-
-
+app.use(cors());
 app.use(express.json());
 
 // Route for home page
-app.get('/', (req, res) => {
-  res.send('Welcome to the Review App!'); // Add your welcome message here
+app.get("/", (req, res) => {
+  res.send("Welcome to the Review App!"); // Add your welcome message here
 });
 
-app.get("/hero", (req, res) => {
+// Route for home page
+app.get("/hh", (req, res) => {
   res.send("Welcome!"); // Add your welcome message here
 });
 
-
-
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
 );
-
